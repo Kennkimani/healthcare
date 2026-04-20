3c9bf45d50d33f133f06a3979357d8b16314eab4
Kennkimani <146022345+Kennkimani@users.noreply.github.com>
2026-04-20 17:05:34 +0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,82 @@
+
+from fastapi import FastAPI,HTTPException
+from pydantic import BaseModel,Field
+from fastapi.middleware.cors import CORSMiddleware
+import joblib
+import pandas as pd
+
+app = FastAPI(title='HealthCare Model Is Running')
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+MODEL_PATH = "C:/Users/PC/3D Objects/healthcare project/model.pkl"
+# Load model + encoders
+model = joblib.load("model.pkl")
+
+
+#  NO target column here
+class PatientData(BaseModel):
+    Name: str
+    Age: float = Field(ge=0, le=120)
+    Gender: str
+    Blood_Type: str
+    Medical_Condition: str
+    Doctor: str
+    Hospital: str
+    Insurance_Provider: str
+    Billing_Amount: float
+    Room_Number: int
+    Admission_Type: str
+    Medication: str
+    Days_in_hospital: int
+    
+
+@app.get("/")
+def home():
+    return {'message':'READY TO MAKE PREDICTIONS?'}
+
+@app.post("/reload")
+def reload_model():
+    """Endpoint for Airflow to trigger a model refresh."""
+    global model
+    try:
+        model = joblib.load(MODEL_PATH)
+        return {"status": "Model reloaded successfully"}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+@app.post("/predict")
+def predict(data: PatientData):
+    try:
+         # Convert input to DataFrame
+        input_df = pd.DataFrame([{
+            "Name":data.Name,
+            "Age": data.Age,
+            "Gender": data.Gender,
+            "Blood Type": data.Blood_Type,
+            "Medical Condition": data.Medical_Condition,
+            "Doctor":data.Doctor,
+            "Hospital":data.Hospital,
+            "Insurance Provider": data.Insurance_Provider,
+            "Billing Amount": data.Billing_Amount,
+            "Room Number": data.Room_Number,
+            "Admission Type": data.Admission_Type,
+            "Medication": data.Medication,
+            "Days_in_hospital": data.Days_in_hospital
+        }])
+
+        # ✅ Predict using pipeline
+        prediction = model.predict(input_df)
+
+        return {
+            "prediction": prediction.tolist(),
+           
+        }
+    except Exception as e:
+        return {"error": str(e)}
+
