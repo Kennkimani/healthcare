b00e119951921276c45d29886978689b343b850c
Kennkimani <146022345+Kennkimani@users.noreply.github.com>
2026-04-21 10:50:54 +0300

    Update script.js

--- static/script.js
+++ static/script.js
@@ -24,7 +24,7 @@ document.getElementById('predictionForm').addEventListener('submit', async (e) =
     };
 
     try {
-        const response = await fetch('https://onrender.com', {
+        const response = await fetch('/predict', {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify(payload)
